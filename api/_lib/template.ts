ac17a7974ff835fc55f25022cb460c1066ada90a
Francesco Forconi <f.forconi@gmail.com>
2020-10-12 20:46:30 +0200

    Prima personalizzaione del template

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,104 +7,79 @@ const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const logo = readFileSync(`${__dirname}/../_assets/logo_ff_black.png`).toString('base64');
+const montserrat = readFileSync(`${__dirname}/../_fonts/Montserrat-Black.woff2`).toString('base64');
+const montserrat_italic = readFileSync(`${__dirname}/../_fonts/Montserrat-Black-Italic.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-    let radial = 'lightgray';
+    //let background = 'white';
+    //let foreground = 'black';
+    //let radial = 'lightgray';
 
     if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-        radial = 'dimgray';
+        //background = 'black';
+        //foreground = 'white';
+        //radial = 'dimgray';
     }
     return `
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Vera';
+        font-family: 'Montserrat';
         font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+        font-weight: 900;
+        src: url(data:font/woff2;charset=utf-8;base64,${montserrat})  format("woff2");
+    }
+
+    @font-face {
+        font-family: 'Montserrat-Italic';
+        font-style: italic;
+        font-weight: 900;
+        src: url(data:font/woff2;charset=utf-8;base64,${montserrat_italic})  format("woff2");
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    html {
+        margin: 0;
+        padding: 0;
+    }
 
     body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
+        margin: 0;
+        background: linear-gradient(165deg, rgba(8,174,234,1) 0%, rgba(42,245,152,1) 100%);
         height: 100vh;
         display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
+        flex-direction: column;
+        padding: 5vh;
     }
 
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
+    .title_container {
+        font-family: 'Montserrat-Italic', sans-serif;
+        font-size: ${sanitizeHtml(fontSize)};
+        font-style: italic;
+        font-weight: 900;
+        color: #000;
+        line-height: 1;
+        flex: 1;
     }
 
-    code:before, code:after {
-        content: '\`';
-    }
-
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
+    .title {
+        border-left: 20px solid black;
+        padding: 5vh;
     }
 
     .logo {
-        margin: 0 75px;
+        text-align: right;
     }
 
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
-    
-    .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
+    .logo img {
+        height: 75px;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+    const { text, theme, md, fontSize} = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
@@ -114,33 +89,16 @@ export function getHtml(parsedReq: ParsedRequest) {
         ${getCss(theme, fontSize)}
     </style>
     <body>
-        <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
+        <div class="title_container">
+            <div class="title">
+            ${emojify(
                 md ? marked(text) : sanitizeHtml(text)
             )}
             </div>
         </div>
+        <div class="logo">
+            <img src="data:image/png;base64,${logo}" />
+        </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`
-}
-
-function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
 }
